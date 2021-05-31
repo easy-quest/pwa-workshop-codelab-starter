5892fcefff0655ea05f791582a5165583aa25dcf
Ivan Yastrebov <easy-quest>
2021-06-01 01:00:40 +0300

    ,

--- js/main.js
+++ js/main.js
@@ -1,19 +1,17 @@
-/*
- Copyright 2021 Google LLC
-
- Licensed under the Apache License, Version 2.0 (the "License");
- you may not use this file except in compliance with the License.
- You may obtain a copy of the License at
-
-      http://www.apache.org/licenses/LICENSE-2.0
-
- Unless required by applicable law or agreed to in writing, software
- distributed under the License is distributed on an "AS IS" BASIS,
- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- See the License for the specific language governing permissions and
- limitations under the License.
- */
-
+import swURL from 'sw:../service-worker.js';
+// Зарегистрировать сервис
+if ('serviceWorker' in navigator) {
+  // Дождитесь, пока событие «Load» не блокирует другую работу
+  window.addEventListener('load', async () => {
+    // Попробуйте зарегистрировать работник услуг.
+    try {
+      const reg = await navigator.serviceWorker.register(swURL);
+      console.log('Сервисный работник зарегистрирован! 😎', reg);
+    } catch (err) {
+      console.log('😥 Регистрация обслуживания работника не удалась: ', err);
+    }
+  });
+}
 window.addEventListener('DOMContentLoaded', async () => {
   // настройтеРедактор
   const { Editor } = await import('./app/editor.js');
@@ -24,7 +22,7 @@ window.addEventListener('DOMContentLoaded', async () => {
   new Menu(document.querySelector('.actions'), editor);
 
   // Установите начальное состояние в редакторе
-  const defaultText = `# Добро пожаловать в PWA Edit!\n\nTo leave the editing area, press the \`esc\` key, then \`tab\` or \`shift+tab\`.`;
+  const defaultText = `# Добро пожаловать в PWA Edit!\n\nЧтобы покинуть область редактирования, нажмите \`esc\` , тогда \`tab\` или же \`shift+tab\`.`;
 
   editor.setContent(defaultText);
 });
--- service-worker.js
+++ service-worker.js
@@ -1,15 +1,27 @@
-/*
-Copyright 2021 Google LLC
+// Выберите имя кэша
+const cacheName = 'cache-v1';
+// Перечислите файлы для предопределенности
+const precacheResources = ['/', '/index.html', '/css/style.css', '/js/main.js', '/js/app/editor.js', '/js/lib/actions.js'];
 
- Licensed under the Apache License, Version 2.0 (the "License");
- you may not use this file except in compliance with the License.
- You may obtain a copy of the License at
+// Когда сервисный работник устанавливается, откройте кэш и добавьте ресурсы rockache к нему
+self.addEventListener('install', (event) => {
+     console.log('Service worker install event!');
+     event.waitUntil(caches.open(cacheName).then((cache) => cache.addAll(precacheResources)));
+});
 
-      http://www.apache.org/licenses/LICENSE-2.0
+self.addEventListener('activate', (event) => {
+     console.log('Service worker activate event!');
+});
 
- Unless required by applicable law or agreed to in writing, software
- distributed under the License is distributed on an "AS IS" BASIS,
- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- See the License for the specific language governing permissions and
- limitations under the License.
- */
+// Когда есть запрашивающий запрос входящей извлечения, попробуйте ответить на предварительно продуманный ресурс, в противном случае отступай к сети
+self.addEventListener('fetch', (event) => {
+     console.log('Fetch intercepted for:', event.request.url);
+     event.respondWith(
+          caches.match(event.request).then((cachedResponse) => {
+               if (cachedResponse) {
+                    return cachedResponse;
+               }
+               return fetch(event.request);
+          }),
+     );
+});
