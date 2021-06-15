f929673e044e2770f2be153becce144df5b94a4d
Ivan Yastrebov <easy-quest@mail.ru>
2021-06-15 10:48:52 +0300

    edit main.js

--- js/main.js
+++ js/main.js
@@ -1,53 +1,78 @@
-import swURL from 'sw:../service-worker.js';
-import { openDB } from 'idb';
-// Set up the database
-const db = await openDB('settings-store', 1, {
-  upgrade(db) {
-    db.createObjectStore('settings');
-  },
-});
-// Зарегистрировать сервис
-if ('serviceWorker' in navigator) {
-  // Дождитесь, пока событие «Load» не блокирует другую работу
-  window.addEventListener('load', async () => {
-    // Попробуйте зарегистрировать работник услуг.
-    try {
-      const reg = await navigator.serviceWorker.register(swURL);
-      console.log('Сервисный работник зарегистрирован! 😎', reg);
-    } catch (err) {
-      console.log('😥 Регистрация обслуживания работника не удалась: ', err);
-    }
-  });
-}
-window.addEventListener('DOMContentLoaded', async () => {
-  // настройтеРедактор
-  const { Editor } = await import('./app/editor.js');
-  const editor = new Editor(document.body);
-  // Set up install prompt
-  const { Install } = await import('./lib/install.js');
-  new Install(document.querySelector('#install'));
+/*
+ Copyright 2021 Google LLC
 
-  // Настройте меню
-  const { Menu } = await import('./app/menu.js');
-  new Menu(document.querySelector('.actions'), editor);
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
 
-  // Установите начальное состояние в редакторе
-  const defaultText = `# Добро пожаловать в PWA Edit!\n\nЧтобы покинуть область редактирования, нажмите \`esc\` , тогда \`tab\` или же \`shift+tab\`.`;
-  
+      http://www.apache.org/licenses/LICENSE-2.0
 
-  editor.setContent((await db.get('settings', 'content')) || defaultText);
-});
-// Save content to database on edit
-editor.onUpdate(async (content) => {
-  await db.put('settings', content, 'content');
-});
-// Set up night mode toggle
-const { NightMode } = await import('./app/night-mode.js');
-new NightMode(
-  document.querySelector('#mode'),
-  async (mode) => {
-    editor.setTheme(mode);
-    // Save the night mode setting when changed
-  },
-  // Retrieve the night mode setting on initialization
-);
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
+
+ import { openDB } from 'idb';
+
+ // Need to use this WMR syntax to properly compile the service worker.
+ // If you compile your service worker in another way, you can use the URL to it
+ // directly in navigator.serviceWorker.register
+ import swURL from 'sw:../service-worker.js';
+ 
+ // Register the service worker
+ if ('serviceWorker' in navigator) {
+   // Wait for the 'load' event to not block other work
+   window.addEventListener('load', async () => {
+     // Try to register the service worker.
+     try {
+       const reg = await navigator.serviceWorker.register(swURL);
+       console.log('Service worker registered! 😎', reg);
+     } catch (err) {
+       console.log('😥 Service worker registration failed: ', err);
+     }
+   });
+ }
+ 
+ window.addEventListener('DOMContentLoaded', async () => {
+   // Set up the database
+   const db = await openDB('settings-store', 1, {
+     upgrade(db) {
+       db.createObjectStore('settings');
+     },
+   });
+ 
+   // Set up the editor
+   const { Editor } = await import('./app/editor.js');
+   const editor = new Editor(document.body);
+ 
+   // Set up the menu
+   const { Menu } = await import('./app/menu.js');
+   new Menu(document.querySelector('.actions'), editor);
+ 
+   // Save content to database on edit
+   editor.onUpdate(async (content) => {
+     await db.put('settings', content, 'content');
+   });
+ 
+   // Set the initial state in the editor
+   const defaultText = `# Welcome to PWA Edit!\n\nTo leave the editing area, press the \`esc\` key, then \`tab\` or \`shift+tab\`.`;
+ 
+   editor.setContent((await db.get('settings', 'content')) || defaultText);
+ 
+   // Set up night mode toggle
+   const { NightMode } = await import('./app/night-mode.js');
+   new NightMode(
+     document.querySelector('#mode'),
+     async (mode) => {
+       editor.setTheme(mode);
+       await db.put('settings', mode, 'night-mode');
+     },
+     await db.get('settings', 'night-mode'),
+   );
+ 
+   // Set up install prompt
+   const { Install } = await import('./lib/install.js');
+   new Install(document.querySelector('#install'));
+ });
